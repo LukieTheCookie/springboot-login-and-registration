f52e2e0f53c523ef5ba149716c7c7636a3337dbd
Luqmaan Kamaar <l.kamaar10@gmail.com>
2024-04-15 18:57:19 +0200

    Initial commit

---
+++ src/main/java/com/example/userauth2/RegistrationController.java
@@ -0,0 +1,25 @@
+package com.example.userauth2;
+
+import com.example.userauth2.model.MyUser;
+import com.example.userauth2.model.MyUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+
+    @Autowired
+    private MyUserRepository myUserRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register/user")
+    public MyUser createUser(@RequestBody MyUser user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return myUserRepository.save(user);
+    }
+}
---
+++ src/test/java/com/example/userauth2/UserAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
